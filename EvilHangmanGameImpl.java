80612b7879eb4813d8d3ef87bec1e08ced086400
Derek Carr <zaphinath@gmail.com>
2013-07-08 08:42:48 -0600

    Added gameplay functionality
    Still need to add the evil algorithm and winning function

--- EvilHangmanGameImpl.java
+++ EvilHangmanGameImpl.java
@@ -2,13 +2,31 @@ package hangman;
 
 import java.io.File;
 import java.util.Set;
+import java.util.HashSet;
+//import java.util.List;
+//import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+import java.lang.StringBuilder;
 
 public class EvilHangmanGameImpl implements EvilHangmanGame {
-	
-//	@SuppressWarnings("serial")
-//	public static class GuessAlreadyMadeException extends Exception {
-//	}
-	
+  //private List<Character> guessedChars;	
+  private StringBuilder guessedChars;
+  private HashSet<String> dict;
+	private boolean[] chars;
+  private int remainingGuesses;  
+  private StringBuilder secretWord; // This is the winning world  
+  /* Constructor 
+   * Set up array of 26 to know which chars have been used;
+   */
+  public EvilHangmanGameImpl() {
+    guessedChars = new StringBuilder("Used letters: ");
+    chars = new boolean[26];
+    for (int i = 0; i < 26; i++) {
+      chars[i] = false;
+    }
+  }
+
 	/**
 	 * Starts a new game of evil hangman using words from <code>dictionary</code>
 	 * with length <code>wordLength</code>
@@ -17,7 +35,12 @@ public class EvilHangmanGameImpl implements EvilHangmanGame {
 	 * @param wordLength Number of characters in the word to guess
 	 */
 	public void startGame(File dictionary, int wordLength) {
-
+    loadDictionary(dictionary, wordLength);
+    secretWord = new StringBuilder(wordLength);
+    for (int i = 0; i < wordLength; i++) {
+      secretWord.append('-');
+    }
+  
   }
 	
 
@@ -33,7 +56,69 @@ public class EvilHangmanGameImpl implements EvilHangmanGame {
 	 * has already been guessed in this game.
 	 */
 	public Set<String> makeGuess(char guess) throws GuessAlreadyMadeException {
+    HashSet setSets = new HashSet<HashSet<String>>();
     return null;
   }
+
+  public char getCharInput() {
+    // make it lowercase
+    // if used letter already prompt again to get new letter
+    // if invalid input prompt for new input
+    // check if it is a char
+    // add char to array of used chars
+    // append to string guessedChars
+    System.out.print("Enter Guess: ");
+    Scanner in = new Scanner(System.in);
+    char c = (char) in.next().charAt(0);
+    Character ch = new Character(c);
+    ch = Character.toLowerCase(ch);
+    int numValue = Character.getNumericValue(ch) - 10;
+    if (numValue < 0 || numValue > 26) {
+      System.out.println("Invalid Input");
+      getCharInput();
+    }
+    if (chars[numValue] == true) {
+      System.out.println("You already used that letter.");
+      getCharInput();
+    } else {
+      chars[numValue] = true;
+      guessedChars.append(" " + ch);
+      remainingGuesses--;
+    }
+    return ch;
+  }
+  
+  public void playGame(int numGuesses) {
+    remainingGuesses = numGuesses;
+    for (int i = 0; i < numGuesses; i++) {
+      System.out.println("You have "+remainingGuesses+" guesses left");
+      System.out.println(guessedChars.toString());
+      System.out.println("Word: " + secretWord.toString());
+      char guess = getCharInput();
+      System.out.println(guess); 
+    }
+  }
+
+  private void loadDictionary(File dictionary, int wordLength) {
+    try {
+      if (dictionary.length() == 0) {
+        System.out.println("Dictionary Empty");
+        System.exit(-9);
+      }
+      //guessedChars = new ArrayList<Character>();
+      dict = new HashSet<String>();
+      Scanner sc = new Scanner(dictionary);
+      while (sc.hasNext()) {
+        String tmp = sc.next();
+        if (tmp.length() == wordLength) {
+          dict.add(tmp);
+        }
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("No Such File");
+    }
+
+  }
+
 	
 }
--- Hangman.java
+++ Hangman.java
@@ -1,20 +1,27 @@
 package hangman;
 
-
+import java.io.File;
 
 public class Hangman {
   private static String usage = "Usage: java HangMan dictionary wordLength guesses";
+  //private static File dictionary;
+  //private static int wordLength;
+  //private static int guesses;
+
 
   public static void main(String[] args) {
     if (args.length > 3 || args.length < 2) {
       System.out.println(usage);
       System.exit(-9);
     }
-    String dictionary = args[0];
-    String wordLength = args[1];
-    String guesses = args[2];
+    File dictionary = new File(args[0]);
+    int wordLength = Integer.parseInt(args[1]);
+    int guesses = Integer.parseInt(args[2]);
+
+    EvilHangmanGameImpl game = new EvilHangmanGameImpl();
+  
+    game.startGame(dictionary, wordLength);
+    game.playGame(guesses);
   }
-
-  EvilHangmanGame game = new EvilHangmanGameImpl();
-
+  
 }
